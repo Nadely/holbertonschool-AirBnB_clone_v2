01ffa570ef4bfe290b64ade20ad7e09dc60c5b74
letihab <letihabib1822@gmail.com>
2023-12-11 11:53:39 +0100

    add file route to start flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello HBNB!</p>"
+=======
+"""create route"""
+from flask import Flask
+
+
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+>>>>>>> 2d72836 (starts a flask web application)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
