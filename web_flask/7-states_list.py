47443e073666d1ffe66815de3df8ea1c5565d1fe
Nadely <6890@holbertonstudents.com>
2023-12-13 16:14:13 +0100

    add space & description

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Write a script that starts a Flask web application"""
 
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,15 +9,20 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown(exception):
+    """remove the current SQLAlchemy Session"""
     storage.close()
+
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
+    """Return State: <state.id>: <B><state.name></B>"""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
+    """main"""
     app.run(host='0.0.0.0', port=5000)
