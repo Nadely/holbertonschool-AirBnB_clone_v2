2218d9c1ec57ff14bf03d6b7ae7900b8e535724c
Nadely <6890@holbertonstudents.com>
2023-12-13 15:59:24 +0100

    add description

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Write a script that starts a Flask web application"""
 
+
 from flask import Flask
 
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_world():
@@ -13,6 +15,7 @@ def hello_world():
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """ Return HBNB"""
     return "HBNB"
 
 
