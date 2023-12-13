a71d2356fe8af99fecab876e22b363d1cceff1ad
Nadely <6890@holbertonstudents.com>
2023-12-13 16:03:03 +0100

    add space

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,6 +13,7 @@ def hello_world():
     """Return Hello HBNB"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Return HBNB"""
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,10 +13,12 @@ def hello_world():
     """Return Hello HBNB"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """return HBNB"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
